d76c0d4d692d993346a3d15db5021e099ef5af7d
Dann <vmakichyan0@gmail.com>
2023-06-22 13:31:39 +0400

    update

--- client/src/canvas/player/player-controllers.ts
+++ client/src/canvas/player/player-controllers.ts
@@ -8,11 +8,12 @@ import { percentFrom, percentOf } from "../utils/percentage"
 import { DayInfo } from "../../socket/events"
 
 export class PlayerControllers {
-  readonly containerSize = new Size(200, 150)
+  readonly containerSize = new Size(140, 115)
   private arrow: Konva.Image
   private controllersGroup: Konva.Group
   private autofood: Konva.Image
   private lastSentCraftOpen = 0
+  private gap = 5
 
   constructor(readonly player: Player, readonly dayInfo: DayInfo) {
     this.draw()
@@ -24,7 +25,7 @@ export class PlayerControllers {
       this.player.layer.getStage().width() -
         this.player.leaderboard.size.width -
         this.containerSize.width -
-        20,
+        5,
       15
     )
   }
@@ -35,22 +36,26 @@ export class PlayerControllers {
       ...this.position(),
     })
 
-    const timerGroup = new Konva.Group({ x: this.containerSize.width - 100 })
+    const iconSize = new Size(60, 60)
+
+    const timerGroup = new Konva.Group({
+      x: iconSize.width + this.gap,
+    })
     const timerBg = new Konva.Image({
       image: loadImage("images/day-night.png", (img) =>
         timerBg.image(img).cache()
       ),
-      width: 100,
-      height: 100,
+      width: iconSize.width,
+      height: iconSize.height,
     }).cache()
     const timerArrow = new Konva.Image({
       image: loadImage("images/day-night-arrow.png", (img) =>
         timerArrow.image(img).cache()
       ),
-      width: 50,
-      height: 16,
-      x: 50,
-      y: 50,
+      width: 31,
+      height: 13,
+      x: iconSize.width / 2,
+      y: iconSize.height / 2,
       // offsetX: 25,
       offsetY: 8,
       rotation:
@@ -68,10 +73,8 @@ export class PlayerControllers {
       image: loadImage("images/auto-food.png", (img) =>
         this.autofood.image(img).cache()
       ),
-      height: 100,
-      width: 100,
-      x: 100,
-      y: 100,
+      height: iconSize.height,
+      width: iconSize.width,
       visible: false,
     }).cache()
     this.autofood.listening(false)
@@ -83,8 +86,10 @@ export class PlayerControllers {
       image: loadImage("/images/craft-book.png", (img) => {
         craftBook.image(img).cache()
       }),
-      height: 100,
-      width: 100,
+      height: iconSize.height,
+      width: iconSize.width,
+      x: iconSize.width + this.gap,
+      y: iconSize.height + this.gap,
     }).cache()
     const craftGroup = new Konva.Group({
       name: "no-click",
