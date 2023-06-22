a08b55b2644107bd017f0cd136b7f675dd5bc5b6
Dann <vmakichyan0@gmail.com>
2023-06-22 20:43:37 +0400

    furnace highlight

--- src/data/settable/furnace_89.ts
+++ src/data/settable/furnace_89.ts
@@ -23,6 +23,7 @@ export default createSettable(89, 'furnace')
     drawOffset: new Point(0, 5),
     noBackground: true,
   })
+  .highlight({ type: 'circle', data: { radius: 230 } })
   .actionable({
     reactRadius: 100,
     draw: {
