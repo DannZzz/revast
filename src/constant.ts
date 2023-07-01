30dc9410fe858f4ab5729c4e43a5511545c82e40
Dann <vmakichyan0@gmail.com>
2023-07-01 17:10:46 +0400

    mob speed

--- src/constant.ts
+++ src/constant.ts
@@ -63,7 +63,7 @@ export const MAX_SCREEN_SIZE = new Size(2540, 2000)
 export const CLAN_MAX_MEMBERS_SIZE = 9
 export const CLAN_MAX_NAME_SIZE = 20
 
-export const MOB_GLOBAL_SPEED_EFFECT = 75 + 25
+export const MOB_GLOBAL_SPEED_EFFECT = 90 + 25
 export const MOB_GLOBAL_ATTACK_SPEED_EFFECT = 1.5
 
 export const MOB_DIR_NAMES = ['mobs']
