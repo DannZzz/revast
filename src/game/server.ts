4ebe93c07dd3810d363140dc979e01edfb03ad97
Dann <vmakichyan0@gmail.com>
2023-07-01 16:51:54 +0400

    fps

--- src/game/server.ts
+++ src/game/server.ts
@@ -70,7 +70,7 @@ export class GameServer implements GameProps {
   readonly alivePlayers = new Chest<number, Player>()
   readonly staticItems: StaticItemsHandler = new StaticItemsHandler()
   private _gameLoopInterval: any
-  private _gameFPS = 60
+  private _gameFPS = 120
   _lastFrame: number = Date.now()
   private _FPSInterval = 1000 / this._gameFPS
   readonly leaderboard = new Leaderboard()
